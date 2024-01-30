3858bbe2ff0eaedd86bc3857d745aa330371a486
almacode1703 <testmygitt.2023@gmail.com>
2024-01-30 22:28:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const config = require('dotenv').config();
+const port = process.env.PORT;
+const jsonData = require('./jsonData')
+
+app.use(middleWare)
+
+function middleWare(req, res, next){
+  console.log("MiddleWare Started...")
+  next()
+}
+
+app.get('/',(req, res)=>{
+  res.send("Welcome to my NodeServer")
+})
+
+app.get('/home', (req, res) => {
+  res.send("<center><h1>Welcome to HOME PAGE</h1></center>")
+})
+
+app.get('/api/data', (req, res)=>{
+  res.json(jsonData)
+})
+
+
+
+
+
+app.listen(port,(error)=>{
+  if(error){
+    console.log("Error occured....", error)
+  }
+  console.log(`Server started at port : ${port}`)
+})
